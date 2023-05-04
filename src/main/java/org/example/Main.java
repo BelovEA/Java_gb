c4212d03bb93a7d71b6218af684df7f0f62ef522
BelovEA <vrachea676@gmail.com>
2023-05-04 18:13:16 +0300

    hw_1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int currentNum = 0;
+        int previousNum = 0;
+
+        do {
+            System.out.print("Enter an integer (0 to exit): ");
+            if (scanner.hasNextInt()) {
+                currentNum = scanner.nextInt();
+                if (currentNum > 0 && previousNum < 0) {
+                    sum += currentNum;
+                }
+                previousNum = currentNum;
+            } else {
+                System.out.println("Invalid input. Please enter an integer.");
+                scanner.next(); // consume invalid input
+            }
+        } while (currentNum != 0);
+
+        scanner.close(); // close the Scanner
+
+        System.out.println("Sum of positive integers following a negative integer: " + sum);
+    }
+}
